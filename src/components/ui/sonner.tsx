3e0efc29fb8d5411c03fbaa609e87fc1800ceeb5
Alxn <alxn.drazhev@gmail.com>
2026-01-17 21:59:35 +0200

    chore(uj-005): change toast position

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -15,7 +15,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps['theme']}
       className="toaster group"
-      position="top-right"
+      position="bottom-right"
       richColors
       closeButton
       toastOptions={{
